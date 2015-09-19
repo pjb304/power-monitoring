6209899a58db1c8cd4f22d2dfa4c4c2810e21eca
Philip Basford <pjb@ecs.soton.ac.uk>
2015-09-19 20:00:48 +0100

    Initial python version - still populates the old rrd but also puts into mysql database

---
+++ current-cost.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+from serial import Serial
+from urllib2 import urlopen
+from json import loads
+from bs4 import BeautifulSoup
+from os import system
+from power_database import PowerDatabase
+from datetime import datetime
+
+PORT = "/dev/ttyUSB0"
+BAUDRATE = 57600
+TIMEOUT = 5
+def get_external_temp():
+    try:
+        return loads(urlopen("http://api.openweathermap.org/data/2.5/weather?q=Southampton&units=metric", timeout=2).read())[u'main'][u'temp']
+    except Excpetion:
+        return 0
+
+def connect_receiver():
+    serial_port = Serial(PORT, BAUDRATE, timeout=TIMEOUT)
+    db = PowerDatabase()
+    print "Serial port opened"
+    while(True):
+        try:
+            resp = serial_port.readline()
+            if( None == resp and resp == ""):
+                continue
+	    parsed = BeautifulSoup(resp)
+            if( parsed.msg is None):
+                continue
+            internal_temp = float(parsed.msg.tmpr.text)
+            power = float(parsed.msg.watts.text)
+            print "Power " + str(power)
+            print "Temperature " + str(internal_temp)
+            save_rrd(power, internal_temp)
+            db.store_reading(power, internal_temp, get_external_temp())
+        except Exception as e:
+            print e
+
+
+def save_rrd(power, internal_temp):
+    external_temp = get_external_temp()
+    cmd = "rrdtool update powertemp.rrd N:%s:%s:%s" %(power, internal_temp, external_temp)
+    print cmd
+    system(cmd)
+
+if __name__ ==  "__main__":
+    connect_receiver()
---
+++ power_database.py
@@ -0,0 +1,41 @@
+from configobj import ConfigObj
+from datetime import datetime
+
+import MySQLdb
+
+class PowerDatabase:
+    
+    def __init__(self):
+        self.read_config()
+        self.connect_database()
+
+    def read_config(self):
+        config = ConfigObj("cc-db.ini")
+        self.host = config["host"]
+        self.user = config["user"]
+        self.password = config["password"]
+        self.database = config["database"]
+
+    def connect_database(self):
+        self.db = MySQLdb.connect(
+            host = self.host,
+            user = self.user,
+            passwd = self.password,
+            db = self.database)
+
+    def store_reading(self, power, internal, external):
+        self.store_power("cc128", power)
+        self.store_temperature("cc128", internal)
+        self.store_temperature("openwthr", external)
+
+    def store_power(self, device, reading):
+        cursor = self.db.cursor()
+        cursor.execute("INSERT INTO power_readings (timestamp, device, value) VALUES (%s, %s, %s);", (datetime.now().strftime("%Y-%m-%d %H:%M:%S"), device, reading))
+        cursor.close()
+        self.db.commit()
+
+    def store_temperature(self, device, reading):
+        cursor = self.db.cursor()
+        cursor.execute("INSERT INTO temperature_readings (timestamp, device, value) VALUES (%s, %s, %s);", (datetime.now().strftime("%Y-%m-%d %H:%M:%S"), device, reading))
+        cursor.close()
+        self.db.commit()
