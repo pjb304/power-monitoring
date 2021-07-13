cf41b6bbb56b008b83eac9f4458a5b5493becb0f
Philip Basford <github@basford.org.uk>
2021-07-13 18:50:31 +0100

    2to3 and remove rrd tool

--- current-cost.py
+++ current-cost.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from serial import Serial
-from urllib2 import urlopen
+from urllib.request import urlopen
 from json import loads
 from bs4 import BeautifulSoup
 from os import system
@@ -13,36 +13,36 @@ BAUDRATE = 57600
 TIMEOUT = 5
 def get_external_temp():
     try:
-        return loads(urlopen("http://api.openweathermap.org/data/2.5/weather?q=Southampton&units=metric&APPID=615df34fae24f3b1e0de372fe3f80bfa", timeout=2).read())[u'main'][u'temp']
+        return loads(urlopen("http://api.openweathermap.org/data/2.5/weather?q=Southampton&units=metric&APPID=", timeout=2).read())['main']['temp']
     except Exception:
         return 0
 
 def connect_receiver():
     serial_port = Serial(PORT, BAUDRATE, timeout=TIMEOUT)
     db = PowerDatabase()
-    print "Serial port opened"
+    print("Serial port opened")
     while(True):
         try:
             resp = serial_port.readline()
             if( None == resp and resp == ""):
                 continue
-            parsed = BeautifulSoup(resp)
+            parsed = BeautifulSoup(resp, features="lxml")
             if( parsed.msg is None):
                 continue
             internal_temp = float(parsed.msg.tmpr.text)
             power = float(parsed.msg.watts.text)
-            print "Power " + str(power)
-            print "Temperature " + str(internal_temp)
-            save_rrd(power, internal_temp)
+            print("Power " + str(power))
+            print("Temperature " + str(internal_temp))
+#            save_rrd(power, internal_temp)
             db.store_reading(power, internal_temp, get_external_temp())
         except Exception as e:
-            print e
+            print(e)
 
 
 def save_rrd(power, internal_temp):
     external_temp = get_external_temp()
     cmd = "rrdtool update powertemp.rrd N:%s:%s:%s" %(power, internal_temp, external_temp)
-    print cmd
+    print(cmd)
     system(cmd)
 
 if __name__ ==  "__main__":
